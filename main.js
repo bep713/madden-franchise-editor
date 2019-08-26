61ebaea1a57657e1587cd2d5e59e89cbcdaf944e
bep713 <mpanetta755@gmail.com>
2019-08-25 20:09:02 -0400

    Fixing bug where an error will show up on closing the app.

--- main.js
+++ main.js
@@ -45,7 +45,11 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
-    baseFileWatcher.close();
+
+    if (baseFileWatcher) {
+      baseFileWatcher.close();
+    }
+    
     app.quit();
   })
 
