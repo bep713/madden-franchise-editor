0a714cd567caa9df1d5878ab9d6ea7a4ec7143a9
bep713 <bep7130@gmail.com>
2020-04-19 20:14:03 -0400

    Adding release notes.

--- renderer/js/services/preferencesService.js
+++ renderer/js/services/preferencesService.js
@@ -45,7 +45,8 @@ preferencesService.getPreferenceKeys = function () {
             'appVersions': {
                 '4_0_0': false,
                 '4_1_0': false,
-                '4_1_1': false
+                '4_1_1': false,
+                '4_2_0': false
             }
         }
     }
